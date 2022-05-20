f8d714fd4cdb63ede5abf625296f6cc5b8eeb138
Jorg397 <jgonzalez@utalkto.com>
2022-05-20 11:33:02 -0400

    notifications-messages

--- tickets/static/js/create-ticket/ticketInfo.js
+++ tickets/static/js/create-ticket/ticketInfo.js
@@ -1,56 +1,46 @@
 function makeComment(ticketId) {
+  comment = $("#comment-input");
 
+  if (comment.val().trim() == "") {
+    alert("You cannot leave comment empty");
+    return;
+  }
 
-    comment = $('#comment-input')
+  jsonData = {
+    comment: comment.val(),
+  };
 
-    if ( comment.val().trim() == ''){
-        alert('You cannot leave comment empty');
-        return
-    }
-    
-    jsonData = {
-        'comment' : comment.val(),
-    }
+  $.ajax({
+    type: "POST",
+    url: `/tickets/ticket-comment/${ticketId}`,
+    data: jsonData,
+    headers: {
+      Authorization: "Token 8183c7dee6e75605837af987065b8baf0b36c3a1",
+    },
+    success: function (response) {
+      alert("Message sent successfully");
 
-
-    $.ajax({
-        type: 'POST',
-        url: `/tickets/ticket-comment/${ticketId}`,
-        data: jsonData,
-        headers: {"Authorization": "Token d0610c6848b24e10e7a41b17acd3cf485213da8c"},
-        success: function (response) {
-
-            alert('Message sent successfully');
-
-            afterCommentMade(response);
-            comment.val('')
-
-        },
-        error: function (response) {
-            alert('An error has ocurred with your message, please try again.');
-            console.log(response);
-
-        }
-    });
+      afterCommentMade(response);
+      comment.val("");
+    },
+    error: function (response) {
+      alert("An error has ocurred with your message, please try again.");
+      console.log(response);
+    },
+  });
 }
 
-
 function afterCommentMade(data) {
+  commentsTitle = $("#ticket-comments-title");
 
-    commentsTitle = $('#ticket-comments-title')
+  if (commentsTitle === null) {
+    $("#comments-section").append(
+      `<h4 id="ticket-comments-title">Ticket Comments</h4>`
+    );
+  }
 
-    if (commentsTitle === null) {
-
-        $('#comments-section').append(
-            `<h4 id="ticket-comments-title">Ticket Comments</h4>`
-        )
-
-    }
-
-
-    $('#comments-made').prepend(
-
-        `<hr>
+  $("#comments-made").prepend(
+    `<hr>
 
         <p><strong>Created at : Just few seconds ago</strong></p>
         <p><strong>Made by : ${data.made_by}</strong></p>
@@ -59,7 +49,5 @@ function afterCommentMade(data) {
         <p>${data.comment.comment}</p>
 
         <hr>`
-
-    )
-
+  );
 }
