acc19ce41c13ebb75a9199d7c6d9e47071fa1190
Jorg397 <jgonzalez@utalkto.com>
2022-05-20 12:29:27 -0400

    "confirm beauty"

--- tickets/static/js/general/main.js
+++ tickets/static/js/general/main.js
@@ -101,3 +101,22 @@ function afterSendMessage(success) {
 function setTenantInModalTabId(tenant_id) {
   $("#input-tenant-id").val(tenant_id);
 }
+
+$("#button_delete").click(() => {
+  bootbox.confirm({
+    title: "Destroy planet?",
+    message:
+      "Do you want to activate the Deathstar now? This cannot be undone.",
+    buttons: {
+      cancel: {
+        label: '<i class="fa fa-times"></i> Cancel',
+      },
+      confirm: {
+        label: '<i class="fa fa-check"></i> Confirm',
+      },
+    },
+    callback: function (result) {
+      console.log("This was logged in the callback: " + result);
+    },
+  });
+});
