e965c9005ce8fdfc28925dc8eeada2891e6f2d8f
i27ae15 <andresruse18@gmail.com>
2022-05-25 10:21:07 -0400

    watson test

--- watson/views.py
+++ watson/views.py
@@ -2,19 +2,31 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from app_modules.send_email import SendEmail
+from properties.models import Tenants
+from properties.serializers import TenantSerializer
 
 
 class WatsonApi(APIView):
-
-
-
-    def get(self, request):
-
-        return Response({'success':True})
-
-        
     
     def post(self, request):
+        
+        
+        tenant_id = int(request.data['tenant_id'])
+        
+        
+        try:
+            tenant = Tenants.objects.get(id=tenant_id)
+        except:
+            return Response(
+                {
+                    'error': True,
+                    'message': 'Tenant id provided does not exist'
+                })
+            
+        
+        serializer = TenantSerializer(tenant)
+        
+        return Response(serializer.data)
         
         print('----------------------------------')
         print('----------------------------------')
