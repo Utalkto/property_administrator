e75446b2f49c34ab888172686f5258c67f02a033
i27ae15 <andresruse18@gmail.com>
2022-05-27 12:41:52 -0400

    add routes for payments and expenses

--- watson/admin.py
+++ watson/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from .models import Product, UserEmail
 
-<<<<<<< HEAD
-# admin.site.register(Product)
-=======
 admin.site.register(Product)
 admin.site.register(UserEmail)
 
 
->>>>>>> e0b353013b607ec40791a098bdfc95a60b9cfcde
 
 # Register your models here.
