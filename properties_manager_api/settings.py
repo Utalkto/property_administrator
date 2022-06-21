f850df1906719a8c69e9487987070e1e513ae905
i27ae15 <58411306+i27ae15@users.noreply.github.com>
2022-06-21 15:53:35 -0400

    Update settings.py

--- properties_manager_api/settings.py
+++ properties_manager_api/settings.py
@@ -63,7 +63,8 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://154.12.240.122",
     "http://localhost:3000",
-    "http://app.kumbio.com"
+    "http://app.kumbio.com",
+    "https://kumbio-beta.netlify.app"
 ]
 
 
