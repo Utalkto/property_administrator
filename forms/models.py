04ee3f07614455ef44f2a78174f408655e509dde
i27ae15 <andresruse18@gmail.com>
2022-07-20 14:50:10 -0400

    update fields in the db

--- forms/models.py
+++ forms/models.py
@@ -19,11 +19,14 @@ class Form(models.Model):
     # --------------------------------------------------
     # fields
     
+    description = models.CharField(max_length=255, default='')
+    
     form_fields = models.JSONField()
     
     link = models.CharField(max_length=255, default='__no_link__')
     
     name = models.CharField(max_length=255)
+    number_of_views = models.IntegerField(default=0)
     
     is_active = models.BooleanField(default=True)
     
